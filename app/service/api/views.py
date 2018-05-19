4c96cd80258d28c977ea6ec3220d1c7ea417af18
YOUARE Corporation <youarecorp@gmail.com>
2018-05-19 13:52:36 +0900

    #1 api 초기 세팅

--- app/service/api/views.py
+++ app/service/api/views.py
@@ -1,14 +1,13 @@
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from user.models import Device
 
 
 @api_view(['POST'])
-@permission_classes((IsAdminUser,))
+# @permission_classes((IsAdminUser,))
 def token_retrieve_view(request):
     device_unique_key = request.data.get('device_unique_key')
     token = ''
